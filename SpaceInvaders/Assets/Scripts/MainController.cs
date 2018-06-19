bdf891aa5cc97597796512278f7d51a85202cb39
seboschtion <sebastian.hug@pm.me>
2018-06-19 14:37:54 +0200

    added basic

---
+++ SpaceInvaders/Assets/Scripts/MainController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class MainController : MonoBehaviour
+{
+    /// <summary>
+    /// The first-person camera being used to render the passthrough camera image (i.e. AR background).
+    /// </summary>
+    public Camera FirstPersonCamera;
+
+    /// <summary>
+    /// A prefab for tracking and visualizing detected planes.
+    /// </summary>
+    public GameObject DetectedPlanePrefab;
+
+    /// <summary>
+    /// A model to place when a raycast from a user touch hits a plane.
+    /// </summary>
+    public GameObject AndyAndroidPrefab;
+
+    /// <summary>
+    /// The rotation in degrees need to apply to model when the Andy model is placed.
+    /// </summary>
+    private const float k_ModelRotation = 180.0f;
+
+    /// <summary>
+    /// A list to hold all planes ARCore is tracking in the current frame. This object is used across
+    /// the application to avoid per-frame allocations.
+    /// </summary>
+    private List<DetectedPlane> m_AllPlanes = new List<DetectedPlane>();
+
+    /// <summary>
+    /// True if the app is in the process of quitting due to an ARCore connection error, otherwise false.
+    /// </summary>
+    private bool m_IsQuitting = false;
+
+    /// <summary>
+    /// The Unity Update() method.
+    /// </summary>
+    public void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        // Hide snackbar when currently tracking at least one plane.
+        Session.GetTrackables<DetectedPlane>(m_AllPlanes);
+        for (int i = 0; i < m_AllPlanes.Count; i++)
+        {
+            if (m_AllPlanes[i].TrackingState == TrackingState.Tracking)
+            {
+                break;
+            }
+        }
+
+        // If the player has not touched the screen, we are done with this update.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                // Instantiate Andy model at the hit pose.
+                var andyObject = Instantiate(AndyAndroidPrefab, hit.Pose.position, hit.Pose.rotation);
+
+                // Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
+                andyObject.transform.Rotate(0, k_ModelRotation, 0, Space.Self);
+
+                // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
+                // world evolves.
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                // Make Andy model a child of the anchor.
+                andyObject.transform.parent = anchor.transform;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Check and update the application lifecycle.
+    /// </summary>
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+}
