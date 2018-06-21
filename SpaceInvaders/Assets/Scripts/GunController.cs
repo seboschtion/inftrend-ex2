dfed79f3ebda79f65aa38b1531ed6a583bab96d6
Linus Basig <linus@basig.me>
2018-06-21 17:45:55 +0200

    Switch to Railgun

--- SpaceInvaders/Assets/Scripts/GunController.cs
+++ SpaceInvaders/Assets/Scripts/GunController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour {
 
+	public Camera GameCamera;
+	public GameObject GunRotator;
+	public GameObject HitBox;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		Ray ray = GameCamera.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit))
+			print ("I'm looking at " + hit.transform.name);
+		else
+			print ("I'm looking at nothing!");
 	}
 }
